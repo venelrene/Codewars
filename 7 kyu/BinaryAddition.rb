00f31ca1f3d869df4aa67c5ced3cc3b928652ca2
venelrene <venelrene@gmail.com>
2018-03-28 00:03:17 -0400

    Spacing

--- 7 kyu/BinaryAddition.rb
+++ 7 kyu/BinaryAddition.rb
@@ -1,7 +1,7 @@
 # Implement a function that adds two numbers together and returns their sum in binary.
 # The conversion can be done before, or after the addition.
 
-The binary number returned should be a string.
+# The binary number returned should be a string.
 
 def add_binary(a,b)
   string = (a + b).to_s(2)
